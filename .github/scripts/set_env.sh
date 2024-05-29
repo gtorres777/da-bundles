8031dd6adb441ab44708b7a1a8b0e5a5969ec6cd
tux <gustavo.torres.d@tecsup.edu.pe>
2024-05-29 18:14:36 -0500

    test envs6

--- .github/scripts/set_env.sh
+++ .github/scripts/set_env.sh
@@ -8,15 +8,26 @@ CONFIG_FILE="${GITHUB_WORKSPACE}/${BUNDLE_PATH}/config/${TARGET_ENV}.yml"
 if [ -f "$CONFIG_FILE" ]; then
   echo "Loading configuration from $CONFIG_FILE"
 
-  export $(grep -v '^#' "$CONFIG_FILE" | xargs)
+  while IFS= read -r line || [ -n "$line" ]; do
+    # Skip empty lines and comments
+    if [[ -z "$line" || "$line" =~ ^# ]]; then
+      continue
+    fi
 
-  echo "Configuration loaded:"
-  # echo "NOTEBOOK_PATH=$notebook_path"
-  # echo "CLUSTER_NAME=$cluster_name"
-  # echo "S3_PATH=$s3_path"
-  # echo "DB_URL=$db_url"
-  printenv
+    # Extract variable name and value
+    var_name=$(echo "$line" | cut -d '=' -f 1)
+    var_value=$(echo "$line" | cut -d '=' -f 2-)
 
+    # Add the BUNDLE_VAR_ prefix to the variable name
+    prefixed_var_name="BUNDLE_VAR_${var_name}"
+
+    # Export the variable
+    export "$prefixed_var_name"="$var_value"
+
+    echo "$prefixed_var_name=$var_value"
+  done < "$CONFIG_FILE"
+
+  echo "Configuration loaded successfully."
 else
   echo "Configuration file $CONFIG_FILE does not exist"
   exit 1
